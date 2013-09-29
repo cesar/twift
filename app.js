5a12dd29205825f769e9bf9ec4ebe7c86f796514
dbartolomei <dbartolomei@172-30-28-214.dynapool.nyu.edu>
2013-09-29 00:11:38 -0400

    etsy intitial integration

--- app.js
+++ app.js
@@ -6,10 +6,12 @@
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
+var index = require('./routes/index');
 var http = require('http');
 var path = require('path');
 var Twitter = require('twitter');
 var util = require('util');
+
 var twitter = new Twitter({
   consumer_key: 'aevBRTLUjmAlv16wgrqG5w',
   consumer_secret: 'qXHcDKI47ez4c150ZjGsdCCU9gcjnl3JC08aNjJw02U',
@@ -46,6 +48,9 @@ app.get('/suggest/:username', function (req, res) {
     console.log(tweet_text_raw);
   });
 });
+
+app.get('/findgift/:keywords', index.findgift);
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
