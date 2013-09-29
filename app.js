de626ccbacaa7a39dd4a6b029a0ce12401d46f4b
dbartolomei <dbartolomei@172-30-28-214.dynapool.nyu.edu>
2013-09-29 01:26:35 -0400

    twitter+alchemy+etsy integration

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ var Twitter = require('twitter');
 var util = require('util');
 
 var twitter = new Twitter({
-  consumer_key: 'aevBRTLUjmAlv16wgrqG5w',
-  consumer_secret: 'qXHcDKI47ez4c150ZjGsdCCU9gcjnl3JC08aNjJw02U',
-  access_token_key: '46003103-ta6jxdKNSppMiaYFfez7oiiwuTR0U3WTLZjmhj1EM',
-  access_token_secret: '5dh7bOuTyxD8QzDyHluBE1Z9BORHZaB9AiPzoVIsN7k'
+	consumer_key: 'aevBRTLUjmAlv16wgrqG5w',
+	consumer_secret: 'qXHcDKI47ez4c150ZjGsdCCU9gcjnl3JC08aNjJw02U',
+	access_token_key: '46003103-ta6jxdKNSppMiaYFfez7oiiwuTR0U3WTLZjmhj1EM',
+	access_token_secret: '5dh7bOuTyxD8QzDyHluBE1Z9BORHZaB9AiPzoVIsN7k'
 });
 var AlchemyAPI = require('alchemy-api');
 var alchemy = new AlchemyAPI('f425552e7eec51eb837a2b4a7fa08a8ced222609');
@@ -38,39 +38,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 }
 
 app.get('/', function(req, res){
 	res.render('index');
 });
 
-app.get('/suggest/:username', function (req, res) {
-  twitter.get('/statuses/user_timeline.json', {screen_name: req.params.username, count:200, include_entitites: true}, function(data) {
-    var tweet_text_raw = '';
-    for (var i = 0; i < data.length; i++) {
-      tweet_text_raw += data[i].text;
-    }
-    var new_data = [];
 
-    alchemy.keywords(tweet_text_raw, {}, function(error, response)
-    	{
-    		//Only add the elements with high relevance.
-    		for(var i = 0; i < response.keywords.length; i++)
-    		{
-    			if(parseFloat(response.keywords[i].relevance, 10) > 0.85)
-    			{
-    				new_data.push(response.keywords[i]);
-    			}
-    		}
-    		console.log(new_data);
-    	});
-  });
-});
-
+app.post('/suggest', index.suggest);
 app.get('/findgift/:keywords', index.findgift);
 
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+	console.log('Express server listening on port ' + app.get('port'));
 });
