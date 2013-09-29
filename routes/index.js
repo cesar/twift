3f21bcaad16e142f9eac64b07e3fb382d0c4a998
Cesar Cruz <cesarcruz@172-30-28-240.dynapool.nyu.edu>
2013-09-29 01:30:00 -0400

    nada

--- routes/index.js
+++ routes/index.js
@@ -17,22 +17,22 @@ exports.findgift = function(req,res,next){
 	console.log(options);
 
 
-var request = http.request(options, function(response) {
-  console.log('STATUS: ' + response.statusCode);
-  // console.log('HEADERS: ' + JSON.stringify(response.headers));
-  response.setEncoding('utf8');
-  response.on('data', function (chunk) {
-    // JSON.parse(chunk);
-    res.send(chunk);
+  var request = http.request(options, function(response) {
+    console.log('STATUS: ' + response.statusCode);
+    // console.log('HEADERS: ' + JSON.stringify(response.headers));
+    response.setEncoding('utf8');
+    response.on('data', function (chunk) {
+      // JSON.parse(chunk);
+      res.send(chunk);
+    });
   });
-});
 
-request.on('error', function(e) {
+  request.on('error', function(e) {
 	// console.log('problem with request: ' + e.message);
-});
+  });
 
-// write data to request body
-request.write('data\n');
-request.write('data\n');
-request.end();
+  // write data to request body
+  request.write('data\n');
+  request.write('data\n');
+  request.end();
 }
