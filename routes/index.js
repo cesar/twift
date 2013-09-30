585f6464a1cd8e9fb38fe8575aa9dc0a55a88370
crzrcn <fernanlink@gmail.com>
2013-09-29 23:06:48 -0400

    routes/index.js refactoring.

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@ var Twitter = require('twitter');
 var util = require('util');
 
 var twitter = new Twitter({
-	consumer_key: process.env.TWITTER_CONSUMER_KEY,
-	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-	access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-	access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 var AlchemyAPI = require('alchemy-api');
 var alchemy = new AlchemyAPI(process.env.ALCHEMY_API_KEY);
@@ -19,25 +19,31 @@ exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
 
-exports.getSuggestions = function(req, res){
-// 	twitter.get('/statuses/user_timeline.json', {screen_name: req.body.user_name, count:200, include_entitites: true}, function(data) 
-//   {
-// 		  var tweet_text_raw = '';
-// 	    for (var i = 0; i < data.length; i++) {
-// 			   tweet_text_raw += data[i].text;
-// 		  }
-// 		var new_data = [];
+/*
+ * GET etsy suggestions
+ */
 
-// 		alchemy.keywords(tweet_text_raw, {}, function(error, response)
+exports.getSuggestions = function(req, res){
+//  twitter.get('/statuses/user_timeline.json', {screen_name: req.params.giftee, count:2, include_entitites: true}, function(data) 
+//   {
+//      var tweet_text_raw = '';
+//      for (var i = 0; i < data.length; i++) {
+//         tweet_text_raw += data[i].text;
+//      }
+//    var new_data = [];
+
+//    alchemy.keywords(tweet_text_raw, {}, function(error, response)
 //     {
-// 				//Only add the elements with high relevance.
-// 				for(var i = 0; i < response.keywords.length; i++)
+//        //Only add the elements with relevance > 0.85.
+//        var REVELANCE_THRESHOLD = 0.85
+// 
+//        for(var i = 0; i < response.keywords.length; i++)
 //         {
-// 					if(parseFloat(response.keywords[i].relevance, 10) > 0.85)
+//          if(parseFloat(response.keywords[i].relevance, 10) > REVELANCE_THRESHOLD)
 //           {
-// 						new_data.push(response.keywords[i]);
-// 					}
-// 				}
+//            new_data.push(response.keywords[i]);
+//          }
+//        }
 
 //   https.get("openapi.etsy.com/v2/treasuries?api_key=oo4naleziqpm5w8c4q592968&keywords="+response.keywords[0].text, function(res) {
 //   console.log("statusCode: ", res.statusCode);
