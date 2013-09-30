09175b617fe3a7a671a622dc48c82e4af16c8cff
crzrcn <fernanlink@gmail.com>
2013-09-29 22:25:30 -0400

    Added env variables.

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,13 @@ var Twitter = require('twitter');
 var util = require('util');
 
 var twitter = new Twitter({
-	consumer_key: 'aevBRTLUjmAlv16wgrqG5w',
-	consumer_secret: 'qXHcDKI47ez4c150ZjGsdCCU9gcjnl3JC08aNjJw02U',
-	access_token_key: '46003103-ta6jxdKNSppMiaYFfez7oiiwuTR0U3WTLZjmhj1EM',
-	access_token_secret: '5dh7bOuTyxD8QzDyHluBE1Z9BORHZaB9AiPzoVIsN7k'
+	consumer_key: process.env.TWITTER_CONSUMER_KEY,
+	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+	access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+	access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 var AlchemyAPI = require('alchemy-api');
-var alchemy = new AlchemyAPI('f425552e7eec51eb837a2b4a7fa08a8ced222609');
+var alchemy = new AlchemyAPI(process.env.ALCHEMY_API_KEY);
 
 /*
  * GET home page.
@@ -19,40 +19,39 @@ exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
 
-exports.suggest = function(req, res){
-	twitter.get('/statuses/user_timeline.json', {screen_name: req.body.user_name, count:200, include_entitites: true}, function(data) 
-  {
-		  var tweet_text_raw = '';
-	    for (var i = 0; i < data.length; i++) {
-			   tweet_text_raw += data[i].text;
-		  }
-		var new_data = [];
+exports.getSuggestions = function(req, res){
+// 	twitter.get('/statuses/user_timeline.json', {screen_name: req.body.user_name, count:200, include_entitites: true}, function(data) 
+//   {
+// 		  var tweet_text_raw = '';
+// 	    for (var i = 0; i < data.length; i++) {
+// 			   tweet_text_raw += data[i].text;
+// 		  }
+// 		var new_data = [];
 
-		alchemy.keywords(tweet_text_raw, {}, function(error, response)
-    {
-				//Only add the elements with high relevance.
-				for(var i = 0; i < response.keywords.length; i++)
-        {
-					if(parseFloat(response.keywords[i].relevance, 10) > 0.85)
-          {
-						new_data.push(response.keywords[i]);
-					}
-				}
+// 		alchemy.keywords(tweet_text_raw, {}, function(error, response)
+//     {
+// 				//Only add the elements with high relevance.
+// 				for(var i = 0; i < response.keywords.length; i++)
+//         {
+// 					if(parseFloat(response.keywords[i].relevance, 10) > 0.85)
+//           {
+// 						new_data.push(response.keywords[i]);
+// 					}
+// 				}
 
+//   https.get("openapi.etsy.com/v2/treasuries?api_key=oo4naleziqpm5w8c4q592968&keywords="+response.keywords[0].text, function(res) {
+//   console.log("statusCode: ", res.statusCode);
+//   console.log("headers: ", res.headers);
 
-        https.get("openapi.etsy.com/v2/treasuries?api_key=oo4naleziqpm5w8c4q592968&keywords="+response.keywords[0].text, function(res) {
-  console.log("statusCode: ", res.statusCode);
-  console.log("headers: ", res.headers);
+//   res.on('data', function(d) {
+//     process.stdout.write(d);
+//   });
 
-  res.on('data', function(d) {
-    process.stdout.write(d);
-  });
-
-}).on('error', function(e) {
-  console.error(e);
-});
+// }).on('error', function(e) {
+//   console.error(e);
+// });
          
-    });
+//     });
 
-  });
+//   });
 }
